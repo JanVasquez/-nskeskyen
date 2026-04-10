53e366b4b732b396283db09ff4c9d68930c0cdf9
pap4skunk <madsengelberthj@gmail.com>
2026-04-10 13:15:48 +0200

    hejsa

--- src/main/java/org/example/oenskeskyen/OenskeskyenApplication.java
+++ src/main/java/org/example/oenskeskyen/OenskeskyenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OenskeskyenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OenskeskyenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OenskeskyenApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/oenskeskyen/OenskeskyenApplicationTests.java
+++ src/test/java/org/example/oenskeskyen/OenskeskyenApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OenskeskyenApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
